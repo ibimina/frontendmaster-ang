c9e98a95ec49ec2be1675abb5233b4eea587ed00
ibimina <96787210+ibimina@users.noreply.github.com>
2023-03-01 16:21:28 +0100

    languages properties

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontendmaster-ang';
+  languages = [
+    {id:1,title:'C#',description:'C# is a programming language',favourite:false},
+    {id:2,title:'Java',description:'Java is a programming language',favourite:false},
+    {id:3,title:'Python',description:'Python is a programming language',favourite:false},
+    {id:4,title:'JavaScript',description:'JavaScript is a programming language',favourite:false},
+    {id:5,title:'C++',description:'C++ is a programming language',favourite:false},
+    {id:6,title:'C',description:'C is a programming language',favourite:false},
+    {id:7,title:'Ruby',description:'Ruby is a programming language',favourite:false},
+    
+  ]
 }
