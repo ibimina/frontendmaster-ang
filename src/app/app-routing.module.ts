cc585d433e9e000a5a0fc81567b214721d1eb2dd
ibimina <96787210+ibimina@users.noreply.github.com>
2023-03-01 12:08:41 +0100

    docs: change path and component value

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+import { CourseComponent } from './about/course.component';
 
 const routes: Routes = [
-  {path:"", component:HomeComponent}
+  { path: "course", component: CourseComponent }
 ];
 
 @NgModule({
