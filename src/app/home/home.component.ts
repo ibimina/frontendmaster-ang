64d80086499c0978dae44c0bacbc968db2c3e29d
ibimina <96787210+ibimina@users.noreply.github.com>
2023-03-01 22:25:17 +0100

    docs: home class

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { Language } from '../common/model/language';
+
+
+const lang: Language = {
+  id: null,
+  title: "",
+  description: "",
+  favourite: false
+}
+@Component({
+  selector: 'app-home',
+  templateUrl: './Home.component.html',
+  styleUrls: ['./Home.component.css']
+})
+export class HomeComponent {
+  selectedLanguage = lang;
+  languages = [
+    { id: 1, title: 'C#', description: 'C# is a programming language', favourite: false },
+    { id: 2, title: 'Java', description: 'Java is a programming language', favourite: false },
+    { id: 3, title: 'Python', description: 'Python is a programming language', favourite: false },
+    { id: 4, title: 'JavaScript', description: 'JavaScript is a programming language', favourite: false },
+    { id: 5, title: 'C++', description: 'C++ is a programming language', favourite: false },
+    { id: 6, title: 'C', description: 'C is a programming language', favourite: false },
+    { id: 7, title: 'Ruby', description: 'Ruby is a programming language', favourite: false },
+
+  ]
+
+  selectLanguage(language: Language) {
+    this.selectedLanguage = language;
+  }
+  deleteLanguage(languageId: number) {
+    this.languages = this.languages.filter(language => language.id !== languageId);
+  }
+}
