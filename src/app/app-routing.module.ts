bce5e0cf91cca9ce6200ff866420fa7b3ba887dd
ibimina <96787210+ibimina@users.noreply.github.com>
2023-03-01 22:06:59 +0100

    docs: changed routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CourseComponent } from './about/course.component';
+import { HomeComponent } from './home/home.component';
+import { FormComponent } from './form/form.component';
 
 const routes: Routes = [
-  { path: "course", component: CourseComponent }
+  { path: "", component: HomeComponent },
+  { path: "form", component: FormComponent }
 ];
 
 @NgModule({
