d0df0cad4746d24061343756c0951f55d919f0c6
ibimina <96787210+ibimina@users.noreply.github.com>
2023-03-01 18:05:14 +0100

    docs: add deleteLanguage method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,19 +15,22 @@ interface Language {
 export class AppComponent {
 
   title = 'Frontendmaster-ang';
-  selectedLanguage:Language| null = null;
+  selectedLanguage: Language | null = null;
   languages = [
-    {id:1,title:'C#',description:'C# is a programming language',favourite:false},
-    {id:2,title:'Java',description:'Java is a programming language',favourite:false},
-    {id:3,title:'Python',description:'Python is a programming language',favourite:false},
-    {id:4,title:'JavaScript',description:'JavaScript is a programming language',favourite:false},
-    {id:5,title:'C++',description:'C++ is a programming language',favourite:false},
-    {id:6,title:'C',description:'C is a programming language',favourite:false},
-    {id:7,title:'Ruby',description:'Ruby is a programming language',favourite:false},
-    
+    { id: 1, title: 'C#', description: 'C# is a programming language', favourite: false },
+    { id: 2, title: 'Java', description: 'Java is a programming language', favourite: false },
+    { id: 3, title: 'Python', description: 'Python is a programming language', favourite: false },
+    { id: 4, title: 'JavaScript', description: 'JavaScript is a programming language', favourite: false },
+    { id: 5, title: 'C++', description: 'C++ is a programming language', favourite: false },
+    { id: 6, title: 'C', description: 'C is a programming language', favourite: false },
+    { id: 7, title: 'Ruby', description: 'Ruby is a programming language', favourite: false },
+
   ]
 
-  selectLanguage(language  : Language){
+  selectLanguage(language: Language) {
     this.selectedLanguage = language;
+  }
+  deleteLanguage(languageId: number) {
+    this.languages = this.languages.filter(language => language.id !== languageId);
   }
 }
