f036bbd9c4f6bcf4b64a2a5284224f5be7fd61bb
ibimina <96787210+ibimina@users.noreply.github.com>
2023-03-03 00:32:09 +0100

    docs: add courses

--- src/app/common/service/course.service.ts
+++ src/app/common/service/course.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Language } from '../model/language';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CourseService {
 
-  constructor() { }
+  courses : Language[] = [
+    { id: '1', title: 'C#', description: 'C# is a programming language', favourite: false },
+    { id: '2', title: 'Java', description: 'Java is a programming language', favourite: false },
+    { id: '3', title: 'Python', description: 'Python is a programming language', favourite: false },
+    { id: '4', title: 'JavaScript', description: 'JavaScript is a programming language', favourite: false },
+    { id: '5', title: 'C++', description: 'C++ is a programming language', favourite: false },
+    { id: '6', title: 'C', description: 'C is a programming language', favourite: false },
+    { id: '7', title: 'Ruby', description: 'Ruby is a programming language', favourite: false },
+
+  ]
 }
